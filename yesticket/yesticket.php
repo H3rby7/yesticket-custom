da6bc45776d7adbc0984ed75be9a87afced30ecd
h3rby7 <herbielutz@gmx.net>
2022-12-14 00:09:43 +0100

    cache clearable, cache time configurable, re-format plugin page code to use html rather than 'echo ...'

--- yesticket/yesticket.php
+++ yesticket/yesticket.php
@@ -31,7 +31,8 @@ if (!function_exists('is_countable')) {
 function getDataCached($get_url) {
     // TODO: make this an option on the Plugin page?
     // TODO: add an 'empty cache' button on the plugin page?
-    $CACHE_TIME_IN_MINUTES = get_option('yesticket_cache_time_in_minutes', 60);
+    $options = get_option('yesticket_settings');
+    $CACHE_TIME_IN_MINUTES = $options['cache_time_in_minutes'];
     $CACHE_KEY = cacheKey($get_url);
 
     // check if we have cached information
@@ -366,11 +367,11 @@ add_shortcode('yesticket_events_cards', 'getYesTicketEventsCards');
 add_shortcode('yesticket_events_list', 'getYesTicketEventsList');
 add_shortcode('yesticket_testimonials', 'getYesTicketTestimonials');
 
-add_option('yesticket_cache_time_in_minutes', 60);
 add_option('yesticket_transient_keys', array());
 
 // WP Backend Plugin Page
 add_action('admin_menu', 'yesticket_pluginpage_wp_menu');
+add_action( 'admin_init', 'yesticket_settings_init' );
 
 function yesticket_pluginpage_wp_menu()
 {
@@ -379,79 +380,144 @@ function yesticket_pluginpage_wp_menu()
 
 function yesticket_pluginpage_init()
 {
-    echo "<style>
+    yesticket_render_feedback();?>
+    <style>
                   .yt-code { background: #fff; padding: 10px; margin: 5px; font-family: monospace; border: 1px solid #eee; font-size: 1.1em; display: inline-block; }
                   h1 { margin-top: 40px; }
                   h2 { margin-top: 30px; }
                   h3 { margin-top: 20px; font-style: italic; }
                   .ml-3 { margin-left: 30px; }
-                </style>";
-    echoDebugInformation();
-    echo "<h1><img src='".plugin_dir_url(__FILE__) . 'img/YesTicket_logo.png'."' height='60' alt='YesTicket Logo'></h1>";
-    echo "<p>YesTicket ist ein Ticketsystem und wir lieben Wordpress - daher hier unser Plugin. Du kannst damit deine zukünftigen Events und Zuschauerstimmen (Testimonials) per Shortcode an beliebige Stellen deiner Seite einbinden. Im Inhaltsteil, in Widgets oder in was auch immer in Wordpress.</p>";
-    echo "<p>Du kannst mehrere Shortcodes in einer Seite verwenden - also z.B. erst die Liste deiner Auftritte, dann Workshops und am Ende dann Zuschauerstimmen.</p>";
+                </style>
+    <h1><img src='<?php echo plugin_dir_url(__FILE__) ?>img/YesTicket_logo.png' height='60' alt='YesTicket Logo'></h1>
+    <p>YesTicket ist ein Ticketsystem und wir lieben Wordpress - daher hier unser Plugin. Du kannst damit deine zukünftigen Events und Zuschauerstimmen (Testimonials) per Shortcode an beliebige Stellen deiner Seite einbinden. Im Inhaltsteil, in Widgets oder in was auch immer in Wordpress.</p>
+    <p>Du kannst mehrere Shortcodes in einer Seite verwenden - also z.B. erst die Liste deiner Auftritte, dann Workshops und am Ende dann Zuschauerstimmen.</p>
     
-    echo "<h2>Shortcodes für deine Events als Liste</h2>";
-    echo "<p>Du benötigst 2 Dinge: deine persönliche <b>Organizer-ID</b> und deinen dazugehörigen <b>Key</b>. Beides findest du direkt zum Kopieren im Adminbereich von YesTicket > Mehr können > YesTicket einfach einbinden: <a href='https://www.yesticket.org/login/de/integration.php#wp-plugin' target='_blank'>https://www.yesticket.org/login/de/integration.php#wp-plugin</a></p>";
-    echo '<p>Das sieht dann z.B. so aus: <span class="yt-code">[yesticket_events organizer="1" key="e4761c1215ff1bd225e22add" type="all" count="5" theme="light"]</span>';
-    echo "<h3>Optionen für Event-Shortcodes</h3>";
-    echo "<h4>Type</h4>";
-    echo "<p class='ml-3'>Mit <b>type</b> kannst du die eine Liste deiner Auftritte, Workshops oder halt Auftritte und Workshops in einer Liste gemischt anzeigen.</p>";
-    echo '<p class="ml-3"><span class="yt-code">type="performance"</span> nur kommende Auftritte<br>';
-    echo '<span class="yt-code">type="workshop"</span> nur kommende Workshops<br>';
-    echo '<span class="yt-code">type="festivals"</span> nur kommende Festivals<br>';
-    echo '<span class="yt-code">type="all"</span> Workshops und Auftritte gemischt</p>';
-    echo "<h4>Count</h4>";
-    echo "<p class='ml-3'>Mit <b>count</b> kannst du die eine Liste begrenzen. Die eingegebene Zahl ist die Maximalzahl, sofern du so viele kommende Events angelegt hast.</p>";
-    echo '<p class="ml-3"><span class="yt-code">count="5"</span> werden maximal 5 kommende Events angezeigt</p>';
-    echo "<h4>Details</h4>";
-    echo "<p class='ml-3'>Mit <b>details</b> kannst du die Beschreibung zu den Events anzeigen, die in YesTicket hinterlegt ist. Die sind per Link auf- und zuklappbar.</p>";
-    echo '<p class="ml-3"><span class="yt-code">details="yes"</span> zeigt den Link zu Aufklappen und die Beschreibung an</p>';
-    echo "<h4>Theme</h4>";
-    echo "<p class='ml-3'>Mit <b>theme</b> kannst du die Farben deinem Layout ein wenig anpassen. Es gibt eine helle und eine dunkle Variante.</p>";
-    echo '<p class="ml-3"><span class="yt-code">theme="light"</span> Buttons sind Hellgrau und passen zu hellen Hintergründen</p>';
-    echo '<p class="ml-3"><span class="yt-code">theme="dark"</span> Buttons sind Dunkelgrau und passen zu dunklen Hintergründen</p>';
-    echo '<p class="ml-3"><span class="yt-code">theme=""</span> Wenn du theme leer angibst, dann bekommst du eine simple Formatierung und Du kannst mit CSS-Formatierungen in deinem Wordpress die Formatierung selbst überschreiben - eher so die Möglichkeit für Webdesigner.</p>';
+    <h2>Shortcodes für deine Events als Liste</h2>
+    <p>Du benötigst 2 Dinge: deine persönliche <b>Organizer-ID</b> und deinen dazugehörigen <b>Key</b>. Beides findest du direkt zum Kopieren im Adminbereich von YesTicket > Mehr können > YesTicket einfach einbinden: <a href='https://www.yesticket.org/login/de/integration.php#wp-plugin' target='_blank'>https://www.yesticket.org/login/de/integration.php#wp-plugin</a></p>
+    <p>Das sieht dann z.B. so aus: <span class="yt-code">[yesticket_events organizer="1" key="e4761c1215ff1bd225e22add" type="all" count="5" theme="light"]</span>
+    <h3>Optionen für Event-Shortcodes</h3>
+    <h4>Type</h4>
+    <p class='ml-3'>Mit <b>type</b> kannst du die eine Liste deiner Auftritte, Workshops oder halt Auftritte und Workshops in einer Liste gemischt anzeigen.</p>
+    <p class="ml-3"><span class="yt-code">type="performance"</span> nur kommende Auftritte<br>
+    <span class="yt-code">type="workshop"</span> nur kommende Workshops<br>
+    <span class="yt-code">type="festivals"</span> nur kommende Festivals<br>
+    <span class="yt-code">type="all"</span> Workshops und Auftritte gemischt</p>
+    <h4>Count</h4>
+    <p class='ml-3'>Mit <b>count</b> kannst du die eine Liste begrenzen. Die eingegebene Zahl ist die Maximalzahl, sofern du so viele kommende Events angelegt hast.</p>
+    <p class="ml-3"><span class="yt-code">count="5"</span> werden maximal 5 kommende Events angezeigt</p>
+    <h4>Details</h4>
+    <p class='ml-3'>Mit <b>details</b> kannst du die Beschreibung zu den Events anzeigen, die in YesTicket hinterlegt ist. Die sind per Link auf- und zuklappbar.</p>
+    <p class="ml-3"><span class="yt-code">details="yes"</span> zeigt den Link zu Aufklappen und die Beschreibung an</p>
+    <h4>Theme</h4>
+    <p class='ml-3'>Mit <b>theme</b> kannst du die Farben deinem Layout ein wenig anpassen. Es gibt eine helle und eine dunkle Variante.</p>
+    <p class="ml-3"><span class="yt-code">theme="light"</span> Buttons sind Hellgrau und passen zu hellen Hintergründen</p>
+    <p class="ml-3"><span class="yt-code">theme="dark"</span> Buttons sind Dunkelgrau und passen zu dunklen Hintergründen</p>
+    <p class="ml-3"><span class="yt-code">theme=""</span> Wenn du theme leer angibst, dann bekommst du eine simple Formatierung und Du kannst mit CSS-Formatierungen in deinem Wordpress die Formatierung selbst überschreiben - eher so die Möglichkeit für Webdesigner.</p>
 
-    echo "<h2>Shortcodes für deine Events als Kacheln bzw. Cards</h2>";
-    echo "<p>Du benötigst 2 Dinge: deine persönliche <b>Organizer-ID</b> und deinen dazugehörigen <b>Key</b>. Beides findest du direkt zum Kopieren im Adminbereich von YesTicket > Mehr können > YesTicket einfach einbinden: <a href='https://www.yesticket.org/login/de/integration.php#wp-plugin' target='_blank'>https://www.yesticket.org/login/de/integration.php#wp-plugin</a></p>";
-    echo '<p>Das sieht dann z.B. so aus: <span class="yt-code">[yesticket_events_cards organizer="1" key="e4761c1215ff1bd225e22add" type="all" details="no" count="5" theme="light"]</span>';
-    echo "<h3>Optionen für Event-Shortcodes</h3>";
-    echo "<h4>Type</h4>";
-    echo "<p class='ml-3'>Mit <b>type</b> kannst du die eine Liste deiner Auftritte, Workshops oder halt Auftritte und Workshops in einer Liste gemischt anzeigen.</p>";
-    echo '<p class="ml-3"><span class="yt-code">type="performance"</span> nur kommende Auftritte<br>';
-    echo '<span class="yt-code">type="workshop"</span> nur kommende Workshops<br>';
-    echo '<span class="yt-code">type="festivals"</span> nur kommende Festivals<br>';
-    echo '<span class="yt-code">type="all"</span> Workshops und Auftritte gemischt</p>';
-    echo "<h4>Count</h4>";
-    echo "<p class='ml-3'>Mit <b>count</b> kannst du die eine Liste begrenzen. Die eingegebene Zahl ist die Maximalzahl, sofern du so viele kommende Events angelegt hast.</p>";
-    echo '<p class="ml-3"><span class="yt-code">count="5"</span> werden maximal 5 kommende Events angezeigt</p>';
-    echo '<p class="ml-3"><span class="yt-code">count="1"</span> bekommt spezielles CSS, sodass das eine Event schön dargestellt wird.</p>';
-    echo "<h4>Grep</h4>";
-    echo "<p class='ml-3'>Mit <b>grep</b> kannst du die Liste der Events über den Titel filtern.</p>";
-    echo '<p class="ml-3"><span class="yt-code">grep="im Bierhaus"</span> werden nur Events angezeigt, die im Event Titel irgendwo die Zeichenfolge "im Bierhaus" enthalten.</p>';
-    echo "<h4>Theme</h4>";
-    echo "<p class='ml-3'>Mit <b>theme</b> kannst du die Farben deinem Layout ein wenig anpassen. Es gibt eine helle und eine dunkle Variante.</p>";
-    echo '<p class="ml-3"><span class="yt-code">theme="light"</span> Buttons sind Hellgrau und passen zu hellen Hintergründen</p>';
-    echo '<p class="ml-3"><span class="yt-code">theme="dark"</span> Buttons sind Dunkelgrau und passen zu dunklen Hintergründen</p>';
-    echo '<p class="ml-3"><span class="yt-code">theme=""</span> Wenn du theme leer angibst, dann bekommst du eine simple Formatierung und Du kannst mit CSS-Formatierungen in deinem Wordpress die Formatierung selbst überschreiben - eher so die Möglichkeit für Webdesigner.</p>';
+    <h2>Shortcodes für deine Events als Kacheln bzw. Cards</h2>
+    <p>Du benötigst 2 Dinge: deine persönliche <b>Organizer-ID</b> und deinen dazugehörigen <b>Key</b>. Beides findest du direkt zum Kopieren im Adminbereich von YesTicket > Mehr können > YesTicket einfach einbinden: <a href='https://www.yesticket.org/login/de/integration.php#wp-plugin' target='_blank'>https://www.yesticket.org/login/de/integration.php#wp-plugin</a></p>
+    <p>Das sieht dann z.B. so aus: <span class="yt-code">[yesticket_events_cards organizer="1" key="e4761c1215ff1bd225e22add" type="all" details="no" count="5" theme="light"]</span>
+    <h3>Optionen für Event-Shortcodes</h3>
+    <h4>Type</h4>
+    <p class='ml-3'>Mit <b>type</b> kannst du die eine Liste deiner Auftritte, Workshops oder halt Auftritte und Workshops in einer Liste gemischt anzeigen.</p>
+    <p class="ml-3"><span class="yt-code">type="performance"</span> nur kommende Auftritte<br>
+    <span class="yt-code">type="workshop"</span> nur kommende Workshops<br>
+    <span class="yt-code">type="festivals"</span> nur kommende Festivals<br>
+    <span class="yt-code">type="all"</span> Workshops und Auftritte gemischt</p>
+    <h4>Count</h4>
+    <p class='ml-3'>Mit <b>count</b> kannst du die eine Liste begrenzen. Die eingegebene Zahl ist die Maximalzahl, sofern du so viele kommende Events angelegt hast.</p>
+    <p class="ml-3"><span class="yt-code">count="5"</span> werden maximal 5 kommende Events angezeigt</p>
+    <p class="ml-3"><span class="yt-code">count="1"</span> bekommt spezielles CSS, sodass das eine Event schön dargestellt wird.</p>
+    <h4>Grep</h4>
+    <p class='ml-3'>Mit <b>grep</b> kannst du die Liste der Events über den Titel filtern.</p>
+    <p class="ml-3"><span class="yt-code">grep="im Bierhaus"</span> werden nur Events angezeigt, die im Event Titel irgendwo die Zeichenfolge "im Bierhaus" enthalten.</p>
+    <h4>Theme</h4>
+    <p class='ml-3'>Mit <b>theme</b> kannst du die Farben deinem Layout ein wenig anpassen. Es gibt eine helle und eine dunkle Variante.</p>
+    <p class="ml-3"><span class="yt-code">theme="light"</span> Buttons sind Hellgrau und passen zu hellen Hintergründen</p>
+    <p class="ml-3"><span class="yt-code">theme="dark"</span> Buttons sind Dunkelgrau und passen zu dunklen Hintergründen</p>
+    <p class="ml-3"><span class="yt-code">theme=""</span> Wenn du theme leer angibst, dann bekommst du eine simple Formatierung und Du kannst mit CSS-Formatierungen in deinem Wordpress die Formatierung selbst überschreiben - eher so die Möglichkeit für Webdesigner.</p>
     
-    echo "<h2>Shortcodes für Zuschauerstimmen</h2>";
-    echo "<p>Du benötigst für deine Testimonials die gleichen 2 Dinge wie oben: deine persönliche <b>Organizer-ID</b> und deinen dazugehörigen <b>Key</b>. Beides findest du direkt zum Kopieren im Adminbereich von YesTicket > Marketing > YesTicket einfach einbinden: <a href='https://www.yesticket.org/login/de/integration.php#wp-plugin' target='_blank'>https://www.yesticket.org/login/de/integration.php#wp-plugin</a></p>";
-    echo '<p>Das sieht dann z.B. so aus: <span class="yt-code">[yesticket_testimonials organizer="1" key="e4361c1215ff1bd225e22add" count="30"]</span></p>';
-    echo "<h3>Optionen für Testimonial-Shortcodes</h3>";
-    echo "<h4>Count</h4>";
-    echo "<p class='ml-3'>Mit <b>count</b> kannst du die eine Liste begrenzen. Die eingegebene Zahl ist die Maximalzahl, sofern du so viele kommende Events angelegt hast.</p>";
-    echo '<p class="ml-3"><span class="yt-code">count="5"</span> werden maximal 5 kommende Events angezeigt</p>';
+    <h2>Shortcodes für Zuschauerstimmen</h2>
+    <p>Du benötigst für deine Testimonials die gleichen 2 Dinge wie oben: deine persönliche <b>Organizer-ID</b> und deinen dazugehörigen <b>Key</b>. Beides findest du direkt zum Kopieren im Adminbereich von YesTicket > Marketing > YesTicket einfach einbinden: <a href='https://www.yesticket.org/login/de/integration.php#wp-plugin' target='_blank'>https://www.yesticket.org/login/de/integration.php#wp-plugin</a></p>
+    <p>Das sieht dann z.B. so aus: <span class="yt-code">[yesticket_testimonials organizer="1" key="e4361c1215ff1bd225e22add" count="30"]</span></p>
+    <h3>Optionen für Testimonial-Shortcodes</h3>
+    <h4>Count</h4>
+    <p class='ml-3'>Mit <b>count</b> kannst du die eine Liste begrenzen. Die eingegebene Zahl ist die Maximalzahl, sofern du so viele kommende Events angelegt hast.</p>
+    <p class="ml-3"><span class="yt-code">count="5"</span> werden maximal 5 kommende Events angezeigt</p>
+
+    <form action='options.php' method='post'>
+    <h2>Options</h2>
+    <?php
+    echo settings_fields( 'pluginPage' );
+    echo do_settings_sections( 'pluginPage' );
+    echo submit_button();
+    ?></form><?php
+    echo yesticket_cache_clear_button_render();
 }
 
-function echoDebugInformation() {
-    echo '<h1>DEBUG INFO</h1>';
-    echo '<p>Configured cache timout is ' . get_option('yesticket_cache_time_in_minutes') . ' minutes</p>';
-    $cacheKeys = get_option('yesticket_transient_keys');
-    echo '<p>Active Caches</p><ul>';
+function yesticket_settings_init(  ) { 
+
+	register_setting( 'pluginPage', 'yesticket_settings' );
+
+	add_settings_section(
+		'yesticket_pluginPage_section_cache', 
+		__( 'Cache', 'yesticket' ), 
+		'yesticket_settings_section_callback', 
+		'pluginPage'
+	);
+
+	add_settings_field( 
+		'cache_time_in_minutes', 
+		__( 'Cache Time in MIN', 'yesticket' ), 
+		'yesticket_cache_time_in_minutes_render', 
+		'pluginPage', 
+		'yesticket_pluginPage_section_cache' 
+	);
+}
+
+
+function yesticket_cache_time_in_minutes_render(  ) { 
+
+	$options = get_option( 'yesticket_settings' );
+	?>
+	<input type='number' name='yesticket_settings[cache_time_in_minutes]' min="0" step="1" value='<?php echo $options['cache_time_in_minutes']; ?>'>
+	<?php
+
+}
+
+function yesticket_settings_section_callback(  ) { 
+
+	echo __( 'Configure Cache', 'yesticket' );
+
+}
+
+function yesticket_cache_clear_button_render(  ) {
+    ?>
+        <form action="admin.php?page=yesticket-plugin" method="POST">
+            <input type="hidden" name="clear_cache" value="1">
+            <input type="submit" value="Clear Cache">
+        </form>
+    <?php
+}
+
+function yesticket_clear_cache(  ) {
+    $cacheKeys = get_option( 'yesticket_transient_keys' );
+    update_option( 'yesticket_transient_keys', array() );
     foreach($cacheKeys as $k) {
-        echo '<li>' . $k . '</li>';
+        delete_transient($k);
     }
-    echo '</ul>';
+    yesticket_render_success_message('Cache cleared.');
+}
+
+function yesticket_render_feedback(  ) {
+    if ($_POST['clear_cache']) {
+        yesticket_clear_cache();
+    }
+    if ($_GET['settings-updated']) {
+        yesticket_render_success_message('Configuration updated.');
+    }
+}
+
+function yesticket_render_success_message( $msg ) {
+    ?><p style="background-color: #97ff00; padding: 1rem"><?php echo $msg ?></p><?php
 }
