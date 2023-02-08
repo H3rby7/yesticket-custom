41372dd6fb981cab1c07181e53dc7ada4876d87b
h3rby7 <herbielutz@gmx.net>
2023-02-08 15:28:17 +0100

    event shortcode refractored

--- yesticket/shortcodes/yesticket_events.php
+++ yesticket/shortcodes/yesticket_events.php
@@ -47,38 +47,97 @@ function ytp_render_events($result, $att) {
     $content = "";
     $count = 0;
     foreach ($result as $item) {
-        $add = "";
-        $content .= "<div class='ytp-event-row'>";
-        if ($att["type"]=="all") {
-            $add = " <span class='ytp-event-type'>".ytp_render_eventType($item->event_type)."</span>";
-        }
-        $content .= '<a href="'.$item->yesticket_booking_url.'" target="_blank" class="ytp-button">'.__("Tickets", "yesticket").'</a>';
-        $content .= "<span class='ytp-event-date'>".ytp_render_date_and_time($item->event_datetime)."</span>".$add;
-        $content .= "<span class='ytp-event-name'>".htmlentities($item->event_name)."</span>";
-
-        $content .= "<span class='ytp-event-date'>".htmlentities($item->location_name).", ".htmlentities($item->location_city)."</span>";
-        if (!empty($item->event_urgency_string)) {
-            $content.= "<br><span class='ytp-event-urgency'>".htmlentities($item->event_urgency_string).""."</span>";
-        }
-        if ($att["details"] == "yes") {
-            $details = nl2br(htmlentities($item->event_description));
-            if (!empty($item->event_notes_help)) {
-                $details .= "<h5>".__("Hints", "yesticket")."</h5>".nl2br(htmlentities($item->event_notes_help));
-            }
-            $details .= "<h5>".__("Tickets", "yesticket")."</h5>".htmlentities($item->tickets);
-            $details .= "<h5>".__("Location", "yesticket")."</h5>".htmlentities($item->location_name)."<br>".htmlentities($item->location_street)."<br>".htmlentities($item->location_zip)." ".htmlentities($item->location_city).", ".htmlentities($item->location_state).", ".htmlentities($item->location_country);
-            $content .= '<br><details class="ytp-event-details">
-                            <summary><u class="ytp-event-show-details">'.__("Show details", "yesticket").'</u></summary>
-                            <div>'.$details.'</div><div class="ytp-event-button-row"><a href="'.$item->yesticket_booking_url.'" target="_blank" class="ytp-button-big">'.__("Order Tickets", "yesticket").'</a></div>'."
-                        </details>";
-        }
-        $content .= "</div>\n";
+        $content .= ytp_render_event($item, $att);
         $count++;
         if ($count == (int)$att["count"]) {
             break;
         }
     }
     return $content;
+}
+
+function ytp_render_event($item, $att) {
+    $booking_url = $item->yesticket_booking_url;
+    $ticket_text = __("Tickets", "yesticket");
+    $event_datetime = ytp_render_date_and_time($item->event_datetime);
+    $event_name = htmlentities($item->event_name);
+    $location_name = htmlentities($item->location_name);
+    $location_city = htmlentities($item->location_city);
+    $event_type = "";
+    if ($att["type"]=="all") {
+        $event_type = "<span class='ytp-event-type'>".ytp_render_eventType($item->event_type)."</span>";
+    }
+    $urgency = "";
+    if (!empty($item->event_urgency_string)) {
+        $urgency = "<div class='ytp-event-urgency'><span>".htmlentities($item->event_urgency_string)."</span></div>";
+    }
+    $details = "";
+    if ($att["details"] == "yes") {
+        $details = ytp_render_event_details($item);
+    }
+    return <<<EOD
+    <div class='ytp-event-row'>
+        <div>
+            <a href="'.$booking_url.'" target="_blank" class="ytp-button">$ticket_text</a>
+            $urgency
+            <span class='ytp-event-date'>$event_datetime</span>
+            $event_type
+            <span class='ytp-event-name'>$event_name</span>
+            <span class='ytp-event-location'>$location_name</span>
+            <span class='ytp-event-city'>$location_city</span>
+        </div>
+        $details
+    </div>
+EOD; // !!!! Prior to PHP 7.3, the end identifier EOD must not be indented !!!!
+}
+
+function ytp_render_event_details($item) {
+    $event_description = nl2br(htmlentities($item->event_description));
+    $show_details_text = __("Show details", "yesticket");
+    $hints_heading = __("Hints", "yesticket");
+    $hints_text = nl2br(htmlentities($item->event_notes_help));
+    $tickets_heading = __("Tickets", "yesticket");
+    $ticket_text = htmlentities($item->tickets);
+    $location_heading = __("Location", "yesticket");
+    $location_info = ytp_render_event_details_location_info($item);
+    $booking_url = $item->yesticket_booking_url;
+    $order_ticketes_text = __("Order Tickets", "yesticket");
+    return <<<EOD
+    <details class="ytp-event-details">
+        <summary class="ytp-event-show-details">$show_details_text</summary>
+        <div>
+            <h5>$hints_heading</h5>
+            $hints_text
+            <h5>$tickets_heading</h5>
+            $ticket_text
+            <h5>$location_heading</h5>
+            $location_info
+        </div>
+        <div class="ytp-event-button-row">
+            <a href="$booking_url" target="_blank" class="ytp-button-big">$order_ticketes_text</a>
+        </div>
+    </details>
+EOD; // !!!! Prior to PHP 7.3, the end identifier EOD must not be indented !!!!
+}
+
+function ytp_render_event_details_location_info($item) {
+    $name = htmlentities($item->location_name); //br
+    $street = htmlentities($item->location_street); //br
+    $zip = htmlentities($item->location_zip); //
+    $city = htmlentities($item->location_city); //,
+    $state = htmlentities($item->location_state); //,
+    $country = htmlentities($item->location_country);
+    // TODO: a case for translation actually, to catch country specific formatting!
+    return <<<EOD
+    <div class="ytp-event-details-location">
+        <span class="ytp-event-details-location-name">$name</span>
+        <span class="ytp-event-details-location-street">$street</span>
+        <span class="ytp-event-details-location-zip">$zip</span>
+        <span class="ytp-event-details-location-city">$city</span>
+        <span class="ytp-event-details-location-state">$state</span>
+        <span class="ytp-event-details-location-country">$country</span>
+    </div>
+EOD; // !!!! Prior to PHP 7.3, the end identifier EOD must not be indented !!!!
 }
 
 function ytp_render_eventsHelp() {?>
