88aa8b18e4753f3a547fb015eb137c84029d4ed9
h3rby7 <herbielutz@gmx.net>
2023-02-18 14:12:56 +0100

    add 'test_is_countable'

--- yesticket/tests/yesticket_helpers-test.php
+++ yesticket/tests/yesticket_helpers-test.php
@@ -1,16 +1,17 @@
 <?php
 
-/**
- * Class Test_Sample
- *
- * @package YesTicket
- */
-
-class SampleTest extends WP_UnitTestCase
+class YesTicketHelpersTest extends WP_UnitTestCase
 {
   function test_ytp_getImageUrl()
   {
     $this->assertSame('http://example.org/wp-content/plugins/app/img/', ytp_getImageUrl(''));
     $this->assertSame('http://example.org/wp-content/plugins/app/img/my-image.png', ytp_getImageUrl('my-image.png'));
+  }
+
+  function test_is_countable()
+  {
+    $this->assertTrue(is_countable(array()));
+    $this->assertTrue(is_countable(array("a" => "b")));
+    $this->assertFalse(is_countable(NULL));
   }
 }
