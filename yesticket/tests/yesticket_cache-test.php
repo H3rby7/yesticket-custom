268340dc6c765e4b6bef4f9b1cfb60c59a9761b3
h3rby7 <herbielutz@gmx.net>
2023-02-19 20:27:12 +0100

    start on YesTicketCache::getFromCacheOrFresh

--- yesticket/tests/yesticket_cache-test.php
+++ yesticket/tests/yesticket_cache-test.php
@@ -61,4 +61,35 @@ class YesTicketCacheTest extends WP_UnitTestCase
     $this->assertFalse(get_transient('test-A'));
     $this->assertFalse(get_transient('test-B'));
   }
+
+  function test_getFromCacheOrFresh()
+  {
+    $pre_http_request_filter_has_run = false;
+    $external_call_url = '';
+
+    // No cached items present
+    update_option($this->opt_key, array());
+    add_filter('pre_http_request', function ($preempt, $parsed_args, $url) use ( &$pre_http_request_filter_has_run, &$external_call_url ) {
+      echo 'mocked response used';
+      $pre_http_request_filter_has_run = true;
+      $external_call_url = $url;
+      return array(
+        'headers'     => array(),
+        'cookies'     => array(),
+        'filename'    => null,
+        'response'    => array('code' => WP_Http::OK, 'message' => 'OK'),
+        'status_code' => 200,
+        'success'     => 1,
+        'body'        => '{"a-key": "a-value"}',
+      );
+    }, 10, 3);
+
+    // Call function
+    $result = YesTicketCache::getInstance()->getFromCacheOrFresh('test-url');
+    $this->assertTrue($pre_http_request_filter_has_run, "Should make HTTP call.");
+    $this->assertSame($external_call_url, "test-url", "Called wrong url");
+    $this->assertNotEmpty($result);
+    $this->assertNotEmpty($result->{'a-key'}, "Expect result to match mocked response");
+    $this->assertSame('a-value', $result->{'a-key'}, "Expect result to match mocked response");
+  }
 }
