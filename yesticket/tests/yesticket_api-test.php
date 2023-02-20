d6dad452364ecd1bcd287859db5018c9662d1fc6
h3rby7 <herbielutz@gmx.net>
2023-02-20 12:36:36 +0100

    refractor YesTicketApiTest

--- yesticket/tests/yesticket_api-test.php
+++ yesticket/tests/yesticket_api-test.php
@@ -2,32 +2,6 @@
 
 class YesTicketApiTest extends WP_UnitTestCase
 {
-  private $_cache_mock = null;
-
-  protected function setUp(): void
-  {
-    $_cache_property = new ReflectionProperty(YesTicketApi::class, "cache");
-    $_cache_property->setAccessible(true);
-    $instance = YesTicketApi::getInstance();
-    $this->_cache_mock = $this->getMockBuilder(YesTicketCache::class)
-      ->setMethods(['getFromCacheOrFresh'])
-      ->getMock();
-    $_cache_property->setValue($instance, $this->_cache_mock);
-  }
-
-  protected function tearDown(): void
-  {
-    // TODO
-    $att = array(
-      'env' => 'prod',
-      'api-version' => '',
-      'organizer' => '1',
-      'key' => 'key1',
-      'type' => 'all',
-      'count' => '100',
-    );
-  }
-
   function test_class_exists()
   {
     $this->assertTrue(class_exists("YesTicketApi"));
@@ -41,38 +15,35 @@ class YesTicketApiTest extends WP_UnitTestCase
     $this->assertNotEmpty(YesTicketApi::getInstance());
   }
 
-  private function setOptions($organizer_id, $api_key) {
-    update_option('yesticket_settings_required', array(
-      'organizer_id' => $organizer_id,
-      'api_key' => $api_key,
-    ));
-  }
-
-  private function expect($url)
+  private function initMock($expected_url)
   {
+    $_cache_property = new ReflectionProperty(YesTicketApi::class, "cache");
+    $_cache_property->setAccessible(true);
+    $instance = YesTicketApi::getInstance();
+    $cache_mock = $this->getMockBuilder(YesTicketCache::class)
+      ->setMethods(['getFromCacheOrFresh'])
+      ->getMock();
+    $_cache_property->setValue($instance, $cache_mock);
     $mock_result = 'mocked-body';
-    $this->_cache_mock->expects($this->once())
+    $cache_mock->expects($this->once())
       ->method('getFromCacheOrFresh')
-      ->with($url)
+      ->with($expected_url)
       ->will($this->returnValue($mock_result));
     return $mock_result;
   }
 
-  /**
-   * @covers YesTicketApi::getEvents
-   */
-  function test_getEvents_organizer_and_key_set_in_att()
+  private function run_events($locale = 'en_EN', $att = array(), $opt_organizer = NULL, $opt_key = NULL, $expected)
   {
     // Mock locale
-    add_filter('locale', function () {
-      return 'en_EN';
+    add_filter('locale', function () use (&$locale) {
+      return $locale;
     });
-    $att = array(
-      'organizer' => '1',
-      'key' => 'key1',
-    );
-    // Prepare our mock
-    $mock_result = $this->expect("https://www.yesticket.org/api/v2/events.php?lang=en&organizer=1&key=key1");
+    update_option('yesticket_settings_required', array(
+      'organizer_id' => $opt_organizer,
+      'api_key' => $opt_key,
+    ));
+    // Prepare
+    $mock_result = $this->initMock($expected);
 
     $result = YesTicketApi::getInstance()->getEvents($att);
     $this->assertSame($mock_result, $result);
@@ -81,83 +52,22 @@ class YesTicketApiTest extends WP_UnitTestCase
   /**
    * @covers YesTicketApi::getEvents
    */
-  function test_getEvents_organizer_and_key_set_in_att_locale_de_DE()
+  function test_getEvents()
   {
-    // Mock locale
-    add_filter('locale', function () {
-      return 'de_DE';
-    });
-    $att = array(
-      'organizer' => '2',
-      'key' => 'keyof2',
-    );
-    // Prepare our mock
-    $mock_result = $this->expect("https://www.yesticket.org/api/v2/events.php?lang=de&organizer=2&key=keyof2");
-    
-    $result = YesTicketApi::getInstance()->getEvents($att);
-    $this->assertSame($mock_result, $result);
-  }
-
-  /**
-   * @covers YesTicketApi::getEvents
-   */
-  function test_getEvents_all_set_in_att_env_dev()
-  {
-    // Mock locale
-    add_filter('locale', function () {
-      return 'en_EN';
-    });
-    $att = array(
-      'env' => 'dev',
-      'organizer' => '1',
-      'key' => 'key1',
-      'type' => 'all',
-      'count' => '100',
-    );
-    // Prepare our mock
-    $mock_result = $this->expect("https://www.yesticket.org/dev/api/v2/events.php?count=100&type=all&lang=en&organizer=1&key=key1");
-    
-    $result = YesTicketApi::getInstance()->getEvents($att);
-    $this->assertSame($mock_result, $result);
-  }
-
-  /**
-   * @covers YesTicketApi::getEvents
-   */
-  function test_getEvents_organizer_and_key_set_in_att_api_v1()
-  {
-    // Mock locale
-    add_filter('locale', function () {
-      return 'en_EN';
-    });
-    $att = array(
-      'api-version' => '1',
-      'organizer' => '1',
-      'key' => 'key1',
-    );
-    // Prepare our mock
-    $mock_result = $this->expect("https://www.yesticket.org/api/events-endpoint.php?lang=en&organizer=1&key=key1");
-    
-    $result = YesTicketApi::getInstance()->getEvents($att);
-    $this->assertSame($mock_result, $result);
-  }
-
-  /**
-   * @covers YesTicketApi::getEvents
-   */
-  function test_getEvents_organizer_and_key_set_in_options()
-  {
-    // Mock locale
-    add_filter('locale', function () {
-      return 'en_EN';
-    });
-    $att = array();
-    $this->setOptions('1', 'key1');
-    
-    // Prepare our mock
-    $mock_result = $this->expect("https://www.yesticket.org/api/v2/events.php?lang=en&organizer=1&key=key1");
-    
-    $result = YesTicketApi::getInstance()->getEvents($att);
-    $this->assertSame($mock_result, $result);
+    $base_uri = 'https://www.yesticket.org/api/v2/events.php';
+    // Basic
+    $this->run_events('en_EN', array(), '1', 'key1', "$base_uri?lang=en&organizer=1&key=key1");
+    // locale de_DE
+    $this->run_events('de_DE', array(), '1', 'key1', "$base_uri?lang=de&organizer=1&key=key1");
+    // Different organizer & key
+    $this->run_events('en_EN', array('organizer' => '2', 'key' => 'keyof2'), '1', 'key1', "$base_uri?lang=en&organizer=2&key=keyof2");
+    // env = dev
+    $this->run_events('en_EN', array('env' => 'dev'), '1', 'key1', "https://www.yesticket.org/dev/api/v2/events.php?lang=en&organizer=1&key=key1");
+    // count = 50
+    $this->run_events('en_EN', array('count' => '50'), '1', 'key1', "$base_uri?count=50&lang=en&organizer=1&key=key1");
+    // type = all
+    $this->run_events('en_EN', array('type' => 'all'), '1', 'key1', "$base_uri?type=all&lang=en&organizer=1&key=key1");
+    // api-version = 1
+    $this->run_events('en_EN', array('api-version' => '1'), '1', 'key1', "https://www.yesticket.org/api/events-endpoint.php?lang=en&organizer=1&key=key1");
   }
 }
