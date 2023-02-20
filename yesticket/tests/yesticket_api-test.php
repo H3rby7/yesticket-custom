0a7f569a2af01191525fa79c1e1f5b5893f442bd
h3rby7 <herbielutz@gmx.net>
2023-02-20 17:31:08 +0100

    finish sunshine tests for YesTicketApi

--- yesticket/tests/yesticket_api-test.php
+++ yesticket/tests/yesticket_api-test.php
@@ -38,8 +38,10 @@ class YesTicketApiTest extends WP_UnitTestCase
     return $mock_result;
   }
 
-  private function run_events($locale = 'en_EN', $att = array(), $opt_organizer = NULL, $opt_key = NULL, $expected)
-  {
+  /**
+   * Prepare Mocks, filters and options for call
+   */
+  private function prepare($locale = 'en_EN', $opt_organizer = NULL, $opt_key = NULL, $expected) {
     // Mock locale
     add_filter('locale', function () use (&$locale) {
       return $locale;
@@ -49,9 +51,20 @@ class YesTicketApiTest extends WP_UnitTestCase
       'api_key' => $opt_key,
     ));
     // Prepare
-    $mock_result = $this->initMock($expected);
+    return $this->initMock($expected);
+  }
 
+  private function run_events($locale = 'en_EN', $att = array(), $opt_organizer = NULL, $opt_key = NULL, $expected)
+  {
+    $mock_result = $this->prepare($locale, $opt_organizer, $opt_key, $expected);
     $result = YesTicketApi::getInstance()->getEvents($att);
+    $this->assertSame($mock_result, $result);
+  }
+
+  private function run_testimonials($locale = 'en_EN', $att = array(), $opt_organizer = NULL, $opt_key = NULL, $expected)
+  {
+    $mock_result = $this->prepare($locale, $opt_organizer, $opt_key, $expected);
+    $result = YesTicketApi::getInstance()->getTestimonials($att);
     $this->assertSame($mock_result, $result);
   }
 
@@ -75,5 +88,27 @@ class YesTicketApiTest extends WP_UnitTestCase
     $this->run_events('en_EN', array('type' => 'all'), '1', 'key1', "$base_uri?type=all&lang=en&organizer=1&key=key1");
     // api-version = 1
     $this->run_events('en_EN', array('api-version' => '1'), '1', 'key1', "https://www.yesticket.org/api/events-endpoint.php?lang=en&organizer=1&key=key1");
+  }
+
+  /**
+   * @covers YesTicketApi::getTestimonials
+   */
+  function test_getTestimonials()
+  {
+    $base_uri = 'https://www.yesticket.org/api/v2/testimonials.php';
+    // Basic
+    $this->run_testimonials('en_EN', array(), '1', 'key1', "$base_uri?lang=en&organizer=1&key=key1");
+    // locale de_DE
+    $this->run_testimonials('de_DE', array(), '1', 'key1', "$base_uri?lang=de&organizer=1&key=key1");
+    // Different organizer & key
+    $this->run_testimonials('en_EN', array('organizer' => '2', 'key' => 'keyof2'), '1', 'key1', "$base_uri?lang=en&organizer=2&key=keyof2");
+    // env = dev
+    $this->run_testimonials('en_EN', array('env' => 'dev'), '1', 'key1', "https://www.yesticket.org/dev/api/v2/testimonials.php?lang=en&organizer=1&key=key1");
+    // count = 50
+    $this->run_testimonials('en_EN', array('count' => '50'), '1', 'key1', "$base_uri?count=50&lang=en&organizer=1&key=key1");
+    // type = all
+    $this->run_testimonials('en_EN', array('type' => 'all'), '1', 'key1', "$base_uri?type=all&lang=en&organizer=1&key=key1");
+    // api-version = 1
+    $this->run_testimonials('en_EN', array('api-version' => '1'), '1', 'key1', "https://www.yesticket.org/api/testimonials-endpoint.php?lang=en&organizer=1&key=key1");
   }
 }
