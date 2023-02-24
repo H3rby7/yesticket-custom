5ce052c8c0715514f4bd484a5c2cc3f71b9fa56d
h3rby7 <herbielutz@gmx.net>
2023-02-24 18:46:28 +0100

    yesticket_events_cards shortcode switch to templating

---
+++ yesticket/shortcodes/templates/event_card.php
@@ -0,0 +1,21 @@
+<?php 
+$time = ytp_to_local_datetime($item->event_datetime);
+$ts = $time->getTimestamp();
+?>
+<a href="<?php echo $item->yesticket_booking_url; ?>" target="_blank">
+  <div class="ytp-event-card">
+    <div class="ytp-event-card-image" style="background-image: url('<?php echo $item->event_picture_url; ?>')"></div>
+    <div class="ytp-event-card-text-wrapper">
+      <div class="ytp-event-card-date">
+        <span class="ytp-event-card-month"><?php echo wp_date('M', $ts); ?></span><br>
+        <span class="ytp-event-card-day"><?php echo wp_date('d', $ts); ?></span><br>
+        <span class="ytp-event-card-year"><?php echo wp_date('Y', $ts); ?></span>
+      </div>
+      <div class="ytp-event-card-body">
+        <div class="ytp-event-card-body-fade-out"></div>
+        <small class="ytp-event-card-location"><?php echo htmlentities($item->location_name); ?></small>
+        <strong class="ytp-event-card-title"><?php echo htmlentities($item->event_name); ?></strong>
+      </div>
+    </div>
+  </div>
+</a>
--- yesticket/shortcodes/yesticket_events_cards.php
+++ yesticket/shortcodes/yesticket_events_cards.php
@@ -43,54 +43,12 @@ class YesTicketEventsCards extends YesTicketEventUsingShortcode
                     continue;
                 }
             }
-            $content .= $this->render_single_card($item);
+            $content .= $this->render_template('event_card', compact("item"));
         }
         if (empty($content)) {
             // content could be empty, if everything is filtered by 'grep'
             $content = ytp_render_no_events();
         }
         return $content;
-    }
-
-    /**
-     * Return an event as html card
-     * 
-     * @param object $item one YesTicket Event
-     * 
-     * @return string html for the event as card
-     */
-    private function render_single_card($item)
-    {
-        $time = ytp_to_local_datetime($item->event_datetime);
-        $booking_url = $item->yesticket_booking_url;
-        // picture size is 1200x628 px
-        $picture_url = $item->event_picture_url;
-        $ts = $time->getTimestamp();
-        $month = wp_date('M', $ts);
-        $day = wp_date('d', $ts);
-        $year = wp_date('Y', $ts);
-        $organizer_name = htmlentities($item->organizer_name);
-        $event_name = htmlentities($item->event_name);
-        $location_name = htmlentities($item->location_name);
-        return <<<EOD
-        <a href="$booking_url" target="_blank">
-            <div class="ytp-event-card">
-                <div class="ytp-event-card-image" style="background-image: url('$picture_url')"></div>
-                <div class="ytp-event-card-text-wrapper">
-                    <div class="ytp-event-card-date">
-                        <span class="ytp-event-card-month">$month</span><br>
-                        <span class="ytp-event-card-day">$day</span><br>
-                        <span class="ytp-event-card-year">$year</span>
-                    </div>
-                    <div class="ytp-event-card-body">
-                        <div class="ytp-event-card-body-fade-out"></div>
-                        <small class="ytp-event-card-location">$location_name</small>
-                        <strong class="ytp-event-card-title">$event_name</strong>
-                    </div>
-                </div>
-            </div>
-        </a>
-EOD;
-        // !!!! Prior to PHP 7.3, the end identifier EOD must not be indented and followed by newline !!!!
     }
 }
