e844917ae9e0e050fe544f67d8f261f7de693df4
h3rby7 <herbielutz@gmx.net>
2023-02-24 18:56:05 +0100

    yesticket_events_list shortcode switch to templating

--- yesticket/shortcodes/yesticket_events_list.php
+++ yesticket/shortcodes/yesticket_events_list.php
@@ -37,47 +37,9 @@ class YesTicketEventsList extends YesTicketEventUsingShortcode
     {
         $content = "<ol>\n";
         foreach ($result as $item) {
-            $content .= $this->render_list_item($item, $att);
+            $content .= $this->render_template('event_list_item', compact("item", "att"));
         }
         $content .= "</ol>\n";
         return $content;
-    }
-
-    /**
-     * Return an event as html list item
-     * 
-     * @param object $item one YesTicket Event
-     * 
-     * @return string html for the event as list item
-     */
-    private function render_list_item($item, $att)
-    {
-        $event_date = ytp_render_date($item->event_datetime);
-        $event_time = ytp_render_time($item->event_datetime);
-        $event_name = htmlentities($item->event_name);
-        $location_name = htmlentities($item->location_name);
-        $location_city = htmlentities($item->location_city);
-        $event_type = "";
-        if ($att["type"] == "all") {
-            $event_type = "<li class='ytp-event-list-type'>" . ytp_render_eventType($item->event_type) . "</li>";
-        }
-        $booking = "";
-        if ($att["ticketlink"] == "yes") {
-            $booking .= '<li class="ytp-event-list-tickets"><a href="' . $item->yesticket_booking_url . '" target="_blank">Tickets</a></li>';
-        }
-        return <<<EOD
-        <li class='ytp-event-list-row'>
-            <ul>
-                <li class='ytp-event-list-date'>$event_date</li>
-                <li class='ytp-event-list-time'>$event_time</li>
-                $event_type
-                <li class='ytp-event-list-name'>$event_name</li>
-                <li class='ytp-event-list-location'>$location_name</li>
-                <li class='ytp-event-list-city'>$location_city</li>
-                $booking
-            </ul>
-        </li>
-EOD;
-        // !!!! Prior to PHP 7.3, the end identifier EOD must not be indented and followed by newline !!!!
     }
 }
