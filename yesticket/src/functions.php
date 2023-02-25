b3e8ff46bd9229db53d08b24323127ad39db2738
h3rby7 <herbielutz@gmx.net>
2023-02-25 13:47:51 +0100

    fix plugin loading to log instead of echo

--- yesticket/src/functions.php
+++ yesticket/src/functions.php
@@ -1,6 +1,8 @@
 <?php
 
-echo "YesTicket plugin loading...\n";
+if (true === WP_DEBUG) {
+    error_log("Loading YesTicket plugin ...\n");
+}
 
 include_once("admin/plugin_menu.php");
 include_once("shortcodes/yesticket_events.php");
@@ -10,8 +12,13 @@ include_once("shortcodes/yesticket_testimonials.php");
 
 add_action('init', 'ytp_init_callback');
 
-function ytp_init_callback() {
+function ytp_init_callback()
+{
     wp_register_style('yesticket', plugins_url('ytp-site.css', __FILE__), false, 'all');
     wp_register_style('yesticket-admin', plugins_url('admin/styles.css', __FILE__), false, 'all');
-    load_plugin_textdomain( 'yesticket', false, basename( dirname( __FILE__ ) ) . '/languages/' );
+    load_plugin_textdomain('yesticket', false, basename(dirname(__FILE__)) . '/languages/');
+}
+
+if (true === WP_DEBUG) {
+    error_log("Loading YesTicket plugin loaded ...\n");
 }
