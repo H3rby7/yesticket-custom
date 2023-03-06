79c288564d0e2de4f61883bc987b198b51e6613a
h3rby7 <herbielutz@gmx.net>
2023-03-06 17:12:04 +0100

    add fallback, if '\locale_get_primary_language' function is not available

--- yesticket/src/helpers/api.php
+++ yesticket/src/helpers/api.php
@@ -335,7 +335,17 @@ class Api
      */
     private function getLocale()
     {
-        return \locale_get_primary_language(\get_locale());
+        $lang = get_locale();
+        if (function_exists('\locale_get_primary_language')) {
+            return \locale_get_primary_language($lang);
+        } else {
+            $lang = get_locale();
+            $langUnderscorePos = strpos($lang, "_");
+            if ($langUnderscorePos != false and $langUnderscorePos > -1) {
+                $lang = substr($lang, 0, $langUnderscorePos);
+            }
+            return $lang;
+        }
     }
 
     /**
