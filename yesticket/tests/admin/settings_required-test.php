4a67626bde1acc9efe4fc0f5ea8ab4b109428250
h3rby7 <herbielutz@gmx.net>
2023-03-15 10:27:12 +0100

    SettingsRequiredTest prepare for second test case with 'necesarrySettingsSet=TRUE'

--- yesticket/tests/admin/settings_required-test.php
+++ yesticket/tests/admin/settings_required-test.php
@@ -15,12 +15,7 @@ class SettingsRequiredTest extends \YTP_TranslateTestCase
     $settingsTechnical = new SettingsRequired('yesticket-settings');
     $_SERVER['REQUEST_URI'] = "http://example.org/wp-admin/admin.php?page=yesticket-settings";
     // Expect Translations
-    $this->expectTranslate("Required Settings");
-    $this->expectTranslate("You need two things: your personal <b>organizer-ID</b> and the corresponding <b>Key</b>. Both can be found in your admin area on YesTicket > Marketing > Integrations:");
-    $this->expectTranslate("https://www.yesticket.org/login/en/integration.php#wp-plugin");
-    $this->expectTranslate("Your 'key'");
-    $this->expectTranslate("Your 'organizer-ID'");
-    $this->expectTranslate("Save Changes", "default");
+    $this->makeTranslateExpections();
     // Render
     \ob_start();
     $settingsTechnical->render();
@@ -29,6 +24,25 @@ class SettingsRequiredTest extends \YTP_TranslateTestCase
     $asXML = $this->validateAndGetAsXml($result);
     // Settings Form Assertions
     $formXML = $asXML->xpath("//form[@method='post']")[0];
+    $this->makeFormAssertions($formXML, "wp-admin/admin.php?page=yesticket");
+  }
+
+  function makeTranslateExpections()
+  {
+    $this->expectTranslate("Required Settings");
+    $this->expectTranslate("You need two things: your personal <b>organizer-ID</b> and the corresponding <b>Key</b>. Both can be found in your admin area on YesTicket > Marketing > Integrations:");
+    $this->expectTranslate("https://www.yesticket.org/login/en/integration.php#wp-plugin");
+    $this->expectTranslate("Your 'key'");
+    $this->expectTranslate("Your 'organizer-ID'");
+    $this->expectTranslate("Save Changes", "default");
+  }
+
+  /**
+   * @param \SimpleXMLElement $formXML the form
+   * @param string $referer to expect for _wp_http_referer input field.
+   */
+  function makeFormAssertions($formXML, $referer)
+  {
     // Form element itself
     $this->assertNotEmpty($formXML, "Should have a form with method='post'.");
     $this->assertStringContainsString("wp-admin/options.php", $formXML->xpath("@action")[0]);
@@ -42,7 +56,7 @@ class SettingsRequiredTest extends \YTP_TranslateTestCase
     // _wp_http_referer
     $hiddenInput = $formXML->xpath("input[@name='_wp_http_referer']")[0];
     $this->assertStringContainsString("hidden", $hiddenInput->xpath("@type")[0]);
-    $this->assertStringContainsString("wp-admin/admin.php?page=yesticket", $hiddenInput->xpath("@value")[0]);
+    $this->assertStringContainsString($referer, $hiddenInput->xpath("@value")[0]);
     // _wpnonce
     $hiddenInput = $formXML->xpath("input[@name='_wpnonce']")[0];
     $this->assertStringContainsString("hidden", $hiddenInput->xpath("@type")[0]);
@@ -50,5 +64,4 @@ class SettingsRequiredTest extends \YTP_TranslateTestCase
     // Submit BTN
     $this->assertNotEmpty($formXML->xpath("//input[@type='submit']"), "Form must have a submit input.");
   }
-
 }
